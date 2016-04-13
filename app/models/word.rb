d0c27b0bcd209b315d604cffd37fa37d58599541
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-04-13 15:01:41 +0200

    Changed semantical_categories from String to Array and corrected typo

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,8 @@
 class Word
   include Mongoid::Document
+
   field :word_name, type: String
   field :description, type: String
   field :syntactical_category, type: String
-  field :semantical-categories, type: String
+  field :semantical_categories, type: Array
 end
