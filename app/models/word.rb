12dbc87e8456bae2a4bf30ae484fb676f273babc
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-05-12 15:55:53 +0200

    Now you can go to the next and previous word in show words.

--- app/models/word.rb
+++ app/models/word.rb
@@ -21,4 +21,25 @@ class Word
   validates :syntactical_category_de, presence: true;
   validates :semantical_categories_de, presence: true;
 
+
+
+  def self.get_previous_word(current_word)
+    # mongodb don't allow index function so first i create a normal array words with the ids, this way i can read the index and then i give the mongo word back with this index -1
+    words_mongo = Word.all
+    words = []
+    Word.each{|w| words.push(w.id)}
+    index = words.index(current_word.id)
+    index = index - 1
+    words_mongo[index]
+  end
+
+  def self.get_next_word(current_word)
+    words_mongo = Word.all
+    words = []
+    Word.each{|w| words.push(w.id)}
+    index = words.index(current_word.id)
+    index = index + 1
+    words_mongo[index]
+  end
+
 end
