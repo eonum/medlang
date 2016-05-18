21dad4d06e1c8c6e8f8109001368e2307182fad9
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-05-18 12:42:03 +0200

    Deleted attributes which were twice. Now every word has only one name, description etc. atrribute.

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,23 +3,19 @@ class Word
 
   has_many :categories
 
-  field :name_de, type: String
-  field :name_en, type: String
-  field :description_de, type: String
-  field :description_en, type: String
-  field :syntactical_category_de, type: String
-  field :syntactical_category_en, type: String
-  field :semantical_categories_de, type: Array, :default => nil
-  field :semantical_categories_en, type: Array
+  field :name, type: String
+  field :description, type: String
+  field :syntactical_category, type: String
+  field :semantical_categories, type: Array, :default => nil
 
 
   # OPTIMIZE: if the word allready exist, it should ask the user if he wants to edit the existing word.
-  validates :name_de, uniqueness: {message: "Dieses Wort gibt es bereits." }
+  validates :name, uniqueness: {message: "Dieses Wort gibt es bereits." }
 
-  validates :name_de, presence: true;
-  validates :description_de, presence: true;
-  validates :syntactical_category_de, presence: true;
-  validates :semantical_categories_de, presence: true;
+  validates :name, presence: true;
+  validates :description, presence: true;
+  validates :syntactical_category, presence: true;
+  validates :semantical_categories, presence: true;
 
 
 
