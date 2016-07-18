cfe8cc51d448e75ab4171ab182a933443f408e93
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-07-18 18:24:02 +0200

    Changed category model to only on 'type' attribute

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,6 +9,9 @@ class Category
   field :name_en, type: String
   field :description_de, type: String
   field :description_en, type: String
-  field :type_de, type: String
-  field :type_en, type: String
+  field :category_type, type: String
+
+  # will delete this later, keep it as a reminder.
+  #field :type_de, type: String
+  #field :type_en, type: String
 end
