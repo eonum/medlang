fa1b4761525e566a8b5d9856a6ba7a7940a61881
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-08-09 13:50:01 +0200

    invere_of nil and allow blank name Blank is necessary because otherwise there is a problem with uniqueness

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,16 +4,16 @@ class Word
   include MultiLanguageText
 
 
-  belongs_to :syntactical_category, class_name: "Category", inverse_of: :syntactical_words, :autosave => true
-  has_and_belongs_to_many :semantical_categories, class_name: "Category", inverse_of: :semantical_words, :autosave => true
+  belongs_to :syntactical_category, class_name: 'Category', inverse_of: nil, autosave: true
+  has_and_belongs_to_many :semantical_categories, class_name: 'Category', inverse_of: nil, autosave: true
   field :name_de, type: String
   field :name_en, type: String
   field :description_de, type: String
   field :description_en, type: String
 
   # OPTIMIZE: if the word allready exist, it should ask the user if he wants to edit the existing word.
-  validates :name_de, uniqueness: {message: "t('word_warning_already_exists')"}
-  validates :name_en, uniqueness: {message: "t('word_warning_already_exists')"}
+  validates :name_de, uniqueness: {message: "t('word_warning_already_exists')"},:allow_blank => true
+  validates :name_en, uniqueness: {message: "t('word_warning_already_exists')"},:allow_blank => true
 
 
 
