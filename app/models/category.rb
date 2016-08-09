3bc5e0ddc497599891d96ee2a58d9f284800205a
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-08-09 13:51:56 +0200

    Those relationships are not necessary

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,6 @@ class Category
   include Mongoid::Document
   include MultiLanguageText
 
-  has_many :syntactical_words, class_name: "Word", inverse_of: :syntactical_category, :autosave => true
-  has_and_belongs_to_many :semantical_words, class_name: "Word", inverse_of: :semantical_categories, :autosave => true
 
 
   field :name_de, type: String
