62ef685b321936a56f8600c3622f9a1f6ece85a3
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-08-10 16:47:11 +0200

    Deleted method which are not in use anymore

--- app/models/word.rb
+++ app/models/word.rb
@@ -14,27 +14,4 @@ class Word
   # OPTIMIZE: if the word allready exist, it should ask the user if he wants to edit the existing word.
   validates :name_de, uniqueness: {message: "t('word_warning_already_exists')"},:allow_blank => true
   validates :name_en, uniqueness: {message: "t('word_warning_already_exists')"},:allow_blank => true
-
-
-
-  # those two functions are used for navigating from on word to an other.
-  # can't use the function index directly on mongo objects so i create the simple array words with all IDs to get the current index, afther that i use this index with a operatoin (+/- 1) to return the complete mongo object
-  def self.get_previous_word(current_word)
-
-    mongo_document = Word.all
-    words = []
-    Word.each{|w| words.push(w.id)}
-    index = words.index(current_word.id)
-    index = index - 1
-    mongo_document[index]
-  end
-
-  def self.get_next_word(current_word)
-    mongo_document = Word.all
-    words = []
-    Word.each{|w| words.push(w.id)}
-    index = words.index(current_word.id)
-    index = index + 1
-    mongo_document[index]
-  end
 end
