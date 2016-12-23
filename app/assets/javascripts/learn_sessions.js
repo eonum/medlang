84e7709ea94b2217a0baa73fd64c2e60b9260ec4
Samuel Jaggi <samuel.jaggi@eonum.ch>
2016-12-23 08:50:00 +0100

    Current status of work, need to switch branch

--- app/assets/javascripts/learn_sessions.js
+++ app/assets/javascripts/learn_sessions.js
@@ -6,11 +6,11 @@ $(function(){
 
     $(window).bind("resize",function(){
         console.log($(this).width())
-        if($(this).width() <751){
-            $('[class^="choice"]').removeClass('choice-1 choice-2 choice-3 choice-4').addClass('choice-small')
+        if($(this).width() < 751){
+            $('[class^="col-xs-12"]').removeClass('choice-1 choice-2 choice-3 choice-4').addClass('choice-small')
         }
         else{
-            $('[class^="choice"]').removeClass('choice-small').addClass('choice-1 choice-2 choice-3 choice-4')
+            $('[class^="col-xs-12"]').removeClass('choice-small').addClass('choice-1 choice-2 choice-3 choice-4')
         }
     })
 })
